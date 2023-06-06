e1e01c390e5fd3e7fd6d7a0db912c92dd4489a30
Paras Verma <workparas8800@gmail.com>
2023-06-07 01:48:57 +0530

    Resolved Error

--- SimpleSimulator/EE.py
+++ SimpleSimulator/EE.py
@@ -53,6 +53,7 @@ def execute(inst, PC, halt):
             else:
                 #overflow handle crow
                 reg_val["FLAGS"]=8
+
         elif (A[opcode]=="xor"):
             result= reg_val[reg_2]^reg_val[reg_3]
             if (0<= result<=127):
@@ -98,7 +99,7 @@ def execute(inst, PC, halt):
             reg_val[reg_1]= reg_val[reg_1]<<imm_val
             reg_val["FLAGS"]=0
         PC+=1
-   #TYPE C
+    #TYPE C
     elif ( opcode in C):
         reg_1=reg_codes[inst[10:13]]
         reg_2=reg_codes[inst[13:16]]
@@ -150,7 +151,7 @@ def execute(inst, PC, halt):
         PC+=1
     #TYPE E
     elif ( opcode in E):
-        mem_addr= binarytodec(inst[9:16])
+        mem_addr= inst[9:16]
 
         if (E[opcode]=="jmp"):
             PC= binarytodec (mem_addr)
@@ -159,22 +160,27 @@ def execute(inst, PC, halt):
         elif (E[opcode]=="jlt"):
             if reg_val["FLAGS"]==4:
                 PC= binarytodec (mem_addr)
-
+            else:
+                PC+=1
         elif (E[opcode]=="jgt"):
             if reg_val["FLAGS"]==2:
                 PC= binarytodec (mem_addr)
+            else:
+                PC+=1
 
         elif (E[opcode]=="je"):
             if reg_val["FLAGS"]==1:
                 PC= binarytodec (mem_addr)
-
+            else:
+                PC+=1
+        reg_val["FLAGS"]=0
     elif ( opcode in F):
         halt= True
     return halt ,PC
     
     
 
-
+#reading input from file
 
 A={"00000":"add","00001":"sub","00110":"mul","01010":"xor","01011":"or","01100":"and"}
 
@@ -188,5 +194,5 @@ E={"01111":"jmp","11100":"jlt","11101":"jgt","11111":"je"}
 
 F={"11010":"hlt"}
 
-reg_codes={"000":"R0","001":"R1","010":"R2","011":"R3","100":"R4","101":"R5","110":"R6","FLAGS":"111"}
+reg_codes={"000":"R0","001":"R1","010":"R2","011":"R3","100":"R4","101":"R5","110":"R6","111":"FLAGS"}
 reg_val={"R0":0,"R1":0,"R2":0,"R3":0,"R4":0,"R5":0,"R6":0,"FLAGS":0}
--- simulator/test.py
+++
@@ -1,6 +0,0 @@
-import MEM
-def fetchdata(pc):
-    return MEM.memory[pc]
-print(MEM.memory)
-MEM.memory[2]=200
-print(MEM.memory)
